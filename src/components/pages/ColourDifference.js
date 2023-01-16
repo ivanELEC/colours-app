d6c90413f119782f3e75751c5dc7ef12472c5d11
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-16 22:03:11 +0000

    Basic Colour Difference Page with Material UI elements
    Added Colour Card Components
    Added Text Fields for each Colour Card
    Added onChange functionality for Text Field inputs to record and update the colour of the state of the respective ColourCard
    Need to use Colour Difference library to see difference

---
+++ src/components/pages/ColourDifference.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid, TextField, Button}  from '@material-ui/core';
+import ColourCard from '../common/ColourCard';
+var cd = require('color-difference');
+
+export default function ColourDifference() {
+
+  //state hooks
+  const [colour1, setColour1] = useState("ffffff")
+  const [colour2, setColour2] = useState("000000")
+  
+  //effect hooks
+
+  const useStyles = makeStyles({
+    root: {
+      
+    }
+  });
+
+  //effects 
+
+
+	
+  //functions
+  const handleChange = (event) => {///saves colour hex to hook value depending on colourNo
+	var inputElement = event.target;
+	var elementId = inputElement.id;
+	if(inputElement){
+		if(elementId == "colour-1-input"){
+			console.log(`#${inputElement.value}`);
+			setColour1(inputElement.value);
+		}
+		else if(elementId == "colour-2-input"){
+			console.log(`#${inputElement.value}`);
+			setColour2(inputElement.value);
+		}
+	}
+  }
+
+
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+		<Grid
+			container spacing={2}
+			direction="column"
+			justifyContent="center"
+		>
+			<Grid 
+				container 
+				item
+				direction="row"
+				justifyContent="center"
+			>
+				<Grid
+					container 
+					item
+					direction="column"
+					justiftContent="center"
+					xs={12}
+					sm={6}
+				>
+					<Grid item>
+						<TextField id="colour-1-input" label="Colour 1" variant="standard" onChange={handleChange} />
+					</Grid>
+					<Grid item>
+						<ColourCard
+							colourName="???"
+							artistName="???"
+							colourHex={`#${colour1}`}
+							date="1/1/2023"
+						/>
+					</Grid>
+				</Grid>
+				<Grid
+					container 
+					item
+					direction="column"
+					justiftContent="center"
+					xs={12}
+					sm={6}
+				>
+					<Grid item>
+						<TextField id="colour-2-input" label="Colour 2" variant="standard" onChange={handleChange} />
+					</Grid>
+					<Grid item>
+						<ColourCard
+							colourName="???"
+							artistName="???"
+							colourHex={`#${colour2}`}
+							date="1/1/2023"
+						/>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item>
+				<Button id="calc-difference-btn" variant="contained">Calculate</Button>
+			</Grid>
+		</Grid>
+  </div>
+  );
+}
