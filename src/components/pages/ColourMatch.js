4595abdcf1653fbca7169a68965963dde21464a6
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-16 22:03:11 +0000

    Basic Colour Difference Page with Material UI elements
    Added Colour Card Components
    Added Text Fields for each Colour Card
    Added onChange functionality for Text Field inputs to record and update the colour of the state of the respective ColourCard
    Need to use Colour Difference library to see difference

---
+++ src/components/pages/ColourMatch.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid, TextField, Button, Typography}  from '@material-ui/core';
+import ColourCard from '../common/ColourCard';
+import { getAllocatedColours } from '../../js/utils/colourMatch';
+import PropTypes from 'prop-types';
+
+
+export default ColourMatch = () => {
+	//state hooks
+
+	//effects 
+
+	//functions 
+
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+
+ColourMatch.propTypes = {
+
+};
