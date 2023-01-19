4b8512331c2e828418c728a43a6a6ce329484eca
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-19 19:47:16 +0000

    lint

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -6,23 +6,23 @@ Basic white card with shadow
 */
 
 export default function Card(props) {
-	const useStyles = makeStyles({
-		root: {
-			minWidth: 275,
-			minHeight: 480,
-			maxWidth: 350,
-			fontFamily: "HelveticaLight",
-			fontSize: 16,
-			borderRadius: 4,
-			transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-			backgroundColor: "#ffffff",
-			overflow: "hidden",
-			boxShadow:
+    const useStyles = makeStyles({
+        root: {
+            minWidth: 275,
+            minHeight: 480,
+            maxWidth: 350,
+            fontFamily: "HelveticaLight",
+            fontSize: 16,
+            borderRadius: 4,
+            transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+            backgroundColor: "#ffffff",
+            overflow: "hidden",
+            boxShadow:
                 "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)"
-		}
-	})
+        }
+    })
 
-    const classes = useStyles()
+	const classes = useStyles()
 
 	return <div className={classes.root}>{props.children}</div>
 }
--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -4,60 +4,60 @@ import Grid from "@material-ui/core/Grid"
 import SendIcon from "@material-ui/icons/Send"
 
 export default function Footer(props) {
-	const useStyles = makeStyles({
-		style: {
-			backgroundColor: "#F8F8F8",
-			borderTop: "1px solid #E7E7E7",
-			textAlign: "left",
-			position: "fixed",
-			left: "0",
-			bottom: "0",
-			maxHeight: "30px",
-			width: "100%",
-			padding: "5px"
-		},
-		phantom: {
-			display: "block",
-			maxHeight: "30px",
-			width: "100%",
-			padding: "5px"
-		}
-	})
+    const useStyles = makeStyles({
+        style: {
+            backgroundColor: "#F8F8F8",
+            borderTop: "1px solid #E7E7E7",
+            textAlign: "left",
+            position: "fixed",
+            left: "0",
+            bottom: "0",
+            maxHeight: "30px",
+            width: "100%",
+            padding: "5px"
+        },
+        phantom: {
+            display: "block",
+            maxHeight: "30px",
+            width: "100%",
+            padding: "5px"
+        }
+    })
 
-    const classes = useStyles()
+	const classes = useStyles()
 
 	return (
-		<div>
-			<div className={classes.phantom} />
-			<div className={classes.style}>
-				<Grid container direction="row" justifyContent="space-between" alignItems="center">
-					<Grid item xs={7} md={3} style={{ fontSize: "14px" }}>
-						<div style={{ fontFamily: "HelveticaBold" }}>Chroma</div>{" "}
-						<div style={{ fontFamily: "HelveticaLight" }}>
+        <div>
+            <div className={classes.phantom} />
+            <div className={classes.style}>
+                <Grid container direction="row" justifyContent="space-between" alignItems="center">
+                    <Grid item xs={7} md={3} style={{ fontSize: "14px" }}>
+                        <div style={{ fontFamily: "HelveticaBold" }}>Chroma</div>{" "}
+                        <div style={{ fontFamily: "HelveticaLight" }}>
                             Akash Chohan + Ivan Yohuno, Est. 2020
-						</div>
-					</Grid>
-					<Grid item xs={2} md={8}></Grid>
-					<Grid
-						item
-						xs={3}
-						md={1}
-						container
-						direction="row"
-						justifyContent="space-between"
-						alignItems="center">
-						<Grid item xs={6} />
-						<Grid item xs={6}>
-							<a
-								style={{ color: "#000000", maxHeight: "70px" }}
-								href="mailto:chromamixes@gmail.com"
-								target="_top">
-								<SendIcon />
-							</a>
-						</Grid>
-					</Grid>
-				</Grid>
-			</div>
-		</div>
-	)
+                        </div>
+                    </Grid>
+                    <Grid item xs={2} md={8}></Grid>
+                    <Grid
+                        item
+                        xs={3}
+                        md={1}
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center">
+                        <Grid item xs={6} />
+                        <Grid item xs={6}>
+                            <a
+                                style={{ color: "#000000", maxHeight: "70px" }}
+                                href="mailto:chromamixes@gmail.com"
+                                target="_top">
+                                <SendIcon />
+                            </a>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    )
 }
