94f5baed0dfe1ce1054bdc8a5812687f165046f4
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-27 20:29:59 +0000

    Create Function for Retrieving Similar Colours
    getSimilarColours will return a list of complex objects (colourDiff, colour) in ascending order of colourDiff that are the most similar to a given inputColour from a colourList.
    
    max number of colours in output list = maxColours
    
    max colour difference = maxDifference
    
    Also linted files and started using ColourMatch to test colourDifference functionality

--- src/js/utils/colourMatch.js
+++ src/js/utils/colourMatch.js
@@ -1,5 +1,5 @@
 //utils to handle functions for colour comparison and grouping
-var cd = require('color-difference');
+var cd = require("color-difference")
 
 export function getAllocatedColours(mixData){//flatten and output a list of colours present in mixData
 	let data = mixData.data
@@ -11,6 +11,32 @@ export function getAllocatedColours(mixData){//flatten and output a list of colo
 	return colours
 }
 
-export function getSimilarColours(maxDifference=1, maxColours){//returns a list of colours 
+export function getSimilarColours(colourList, inputColour, maxDifference=100, maxColours=5){
+/*
+	Returns a list of colours similar to inputColour from an input colourList
+	maxDifference is the max difference in scale the colours picked can be (max is 1)
+	maxColours is the max number of colours in the list 
+*/
+
+	let selectedColours = []
+	let sortedColours = []
+
+	sortedColours = colourList.map((colour)=>{
+		let colourDiff = cd.compare(colour, inputColour)
+		return {"colourDiff": colourDiff, "colour": colour}
+	})
+
+	sortedColours.sort((a,b) => {
+		return parseFloat(a.colourDiff) - parseFloat(b.colourDiff)
+	})
+
+	console.log(sortedColours)
+	selectedColours = sortedColours.splice(0,maxColours)
+
+	selectedColours = selectedColours.filter((element) => {
+		return parseFloat(element.colourDiff) <= maxDifference
+	})
+
+	return selectedColours
 	
 }
