86be5940af7d60c12b6c06ceaba4fe94ce926005
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-28 17:41:49 +0000

    Revert "Revert "Remove unnecessary console logs""
    This reverts commit 3b967842e7000c8f778121ff725e3c72a57686fc.

--- src/components/pages/Mix.js
+++ src/components/pages/Mix.js
@@ -60,10 +60,6 @@ export default function Mix() {
 			})
 	}, [id])
 
-	useEffect(() => {
-		console.log("Mix Metadata", mixStates.mixMetadata)
-	}, [mixStates])
-
 	//styles
 	const styles = {
 		fadeInDown: {
