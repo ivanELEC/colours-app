8f9625638ca1562c035cfd4b679c7e473654aad1
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-01-28 17:40:47 +0000

    Revert "Refactor routing into App.js"
    This reverts commit e1b617a7ba60f28505b7603bf45e779e4ddea0f9.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from "react"
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
 import winston from "winston"
-import AppHome from "./components/pages/AppHome"
-import Mix from "./components/pages/Mix"
-import ColourDifference from "./components/pages/ColourDifference"
-import ColourMatch from "./components/pages/ColourMatch"
-import Footer from "./components/navigation/Footer"
+import { WinstonProvider } from "winston-react"
+import Home from "./components/pages/Home"
 import "./App.css"
 
+const logger = winston.createLogger({
+	transports: [new winston.transports.Console()]
+})
+
 function App() {
-	return  (
-		<div>
-			<Router>
-				<Switch><Route exact path="/" component={AppHome} /></Switch>
-				<Switch><Route exact path="/Mix/:id" component={Mix} /></Switch>
-				<Switch><Route exact path="/ColourDifference" component={ColourDifference} /></Switch>
-				<Switch><Route exact path="/ColourMatch" component={ColourMatch} /></Switch>
-				<Footer/>
-			</Router>
-		</div>
+	return (
+		<WinstonProvider logger={logger}>
+			<div>
+				<Home />
+			</div>
+		</WinstonProvider>
 	)
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
 import "./index.css"
 import App from "./App"
-
+import Mix from "./components/pages/Mix"
+import ColourDifference from "./components/pages/ColourDifference"
+import ColourMatch from "./components/pages/ColourMatch"
+import Footer from "./components/navigation/Footer"
 //load fonts
 import "./fonts/HelveticaNeueCyr-Bold.ttf"
 import "./fonts/HelveticaNeueCyr-Light.ttf"
 
-const application = (
+const routing = (
 	<div>
-		<App/>
+		<Router>
+			<Switch><Route exact path="/" component={App} /></Switch>
+			<Switch><Route exact path="/Mix/:id" component={Mix} /></Switch>
+			<Switch><Route exact path="/ColourDifference" component={ColourDifference} /></Switch>
+			<Switch><Route exact path="/ColourMatch" component={ColourMatch} /></Switch>
+			<Footer/>
+		</Router>
 	</div>
 )
 
-ReactDOM.render(application, document.getElementById("root"))
+ReactDOM.render(routing, document.getElementById("root"))
