10ea2a7bfc67e9a379ffc3dfd60d59a56ac860c9
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-02-26 20:36:45 +0000

    Fix styling on Menu/MenuList and provide options

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from "react"
-import { makeStyles } from "@mui/styles"
+import React from "react"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { makeStyles  } from "@mui/styles"
 import { Grid, Menu, MenuItem, Button } from "@mui/material"
 
 export default function Footer() {
-  //hooks
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const [anchorPosCoordinates, setAnchorPosCoordinates] = React.useState({top:0, left:0})
-  const open = Boolean(anchorEl)
+	//hooks
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const [anchorPosCoordinates, setAnchorPosCoordinates] = React.useState({top:0, left:0})
+	const open = Boolean(anchorEl)
   
-  //functions
-  const handleClick = (event) => {
+	//functions
+	const handleClick = (event) => {
 	let targetCoordinates = {
 		top: event.currentTarget.offsetTop,  
 		left: event.currentTarget.offsetLeft
 	}
 	setAnchorPosCoordinates(targetCoordinates)
-    setAnchorEl(event.currentTarget)
-  }
+	setAnchorEl(event.currentTarget)
+	}
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  };
+	const handleClose = () => {
+	setAnchorEl(null)
+	};
 
-  //styling
+    //styling
 	const useStyles = makeStyles({
 		style: {
 			backgroundColor: "#F8F8F8",
@@ -50,63 +51,77 @@ export default function Footer() {
 		}
 	})
 
+	//themes
+	const theme = createTheme({
+		palette: {
+			neutral: {
+			main: '#000000',
+			contrastText: '#ffffff',
+			},
+		},
+	});
+
 	const classes = useStyles()
 
 	return (
-		<div>
-			<div className={classes.spacing} />
-			<div className={classes.phantom} />
-			<div className={classes.style}>
-				<Grid
-					container
-					direction="row"
-					justifyContent="space-between"
-					alignItems="center"
-				>
-					<Grid item xs={7} md={3} style={{ fontSize: "2.4vh" }}>
-						<div style={{ fontFamily: "HelveticaBold" }}>Chroma</div>{" "}
-						<div style={{ fontFamily: "HelveticaLight" }}>
-							Akash Chohan + Ivan Yohuno, Est. 2020
-						</div>
-					</Grid>
-					<Grid item xs={2} md={8}></Grid>
+		<ThemeProvider theme={theme}>
+			<div>
+				<div className={classes.spacing} />
+				<div className={classes.phantom} />
+				<div className={classes.style}>
 					<Grid
-						item
-						xs={3}
-						md={1}
 						container
 						direction="row"
 						justifyContent="space-between"
 						alignItems="center"
 					>
-						<Grid item xs={5} />
-						<Grid item xs={7}>
-						<Button
-							id="footer-menu-button"
-							aria-controls={open ? 'footer-menu' : undefined}
-							aria-haspopup="true"
-							aria-expanded={open ? "true" : undefined}
-							onClick={handleClick}
-							sx={{ zIndex: 1000}}
+						<Grid item xs={7} md={3} style={{ fontSize: "2.4vh" }}>
+							<div style={{ fontFamily: "HelveticaBold" }}>Chroma</div>
+							<div style={{ fontFamily: "HelveticaLight" }}>
+								Akash Chohan + Ivan Yohuno, Est. 2020
+							</div>
+						</Grid>
+						<Grid item xs={2} md={8}></Grid>
+						<Grid
+							item
+							xs={3}
+							md={1}
+							container
+							direction="row"
+							justifyContent="space-between"
+							alignItems="center"
 						>
-							Menu
-						</Button>
-						<Menu
-							id="footer-menu"
-							aria-labelledby="footer-menu-button"
-							anchorEl={anchorEl}
-							open={open}
-							onClose={handleClose}
-							anchorPosition={{ top: anchorPosCoordinates.top, left: anchorPosCoordinates.left }}
-						>
-							<MenuItem onClick={handleClose}>Profile</MenuItem>
-							<MenuItem onClick={handleClose}>My account</MenuItem>
-							<MenuItem onClick={handleClose}>Logout</MenuItem>
-						</Menu>
+							<Grid item xs={5} />
+							<Grid item xs={7}>
+							<Button
+								id="footer-menu-button"
+								aria-controls={open ? 'footer-menu' : undefined}
+								aria-haspopup="true"
+								aria-expanded={open ? "true" : undefined}
+								onClick={handleClick}
+								color="neutral"
+								sx={{fontFamily:  "HelveticaBold" }}
+							>
+								Menu
+							</Button>
+							<Menu
+								id="footer-menu"
+								aria-labelledby="footer-menu-button"
+								anchorEl={anchorEl}
+								open={open}
+								onClose={handleClose}
+								anchorPosition={{ top: anchorPosCoordinates.top, left: anchorPosCoordinates.left }}
+							>
+								<MenuItem sx={{fontFamily: "HelveticaLight" }} onClick={handleClose}>See All Mixes</MenuItem>
+								<MenuItem sx={{fontFamily: "HelveticaLight" }} onClick={handleClose}>Home</MenuItem>
+								<MenuItem sx={{fontFamily: "HelveticaLight" }} onClick={handleClose}>Help</MenuItem>
+								<MenuItem sx={{fontFamily: "HelveticaLight" }} onClick={handleClose}>Contact Us</MenuItem>
+							</Menu>
+							</Grid>
 						</Grid>
 					</Grid>
-				</Grid>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	)
 }
