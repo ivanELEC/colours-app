0dcde3af8e73339d7627f4fa1d4c366e8f472295
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-02-27 17:26:09 +0000

    Add punctuation to help text so it reads better on Screen Narrator

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -135,17 +135,17 @@ export default function Footer() {
 						<br></br>
 						<div id="modal-help-description" className={classes.helpModalText} >
 							<div className={classes.helpModalSubTitle}> How to find Mixes </div>
-							<p>Each mix card clicks through to a page to read more information about a mix and listen to it</p>
-							<p>You can see all mixes by navigating to the Menu and clicking <q>See All Mixes</q></p>
-							<p>The colour grid and colour selector allow you to choose a colour that filters down to mixes of a similar hue</p>
-							<p>You can either click a colour on the colour grid, or type in a hex code (6 characters) in the colour selector text field</p>
+							<p>Each mix card clicks through to a page to read more information about a mix and listen to it.</p>
+							<p>You can see all mixes by navigating to the Menu and clicking <q>See All Mixes.</q></p>
+							<p>The colour grid and colour selector allow you to choose a colour that filters down to mixes of a similar hue.</p>
+							<p>You can either click a colour on the colour grid, or type in a hex code (6 characters) in the colour selector text field.</p>
 							<div className={classes.helpModalSubTitle}> Hex Codes </div>
-							<p>Hex codes are a way of representing R(ed) G(reen) B(lue) colours</p>
-							<p>Hex codes are split into 3 elements which are 2 symbols in length each</p>
-							<p>First element represents RED, second element represents GREEN and the third element represents BLUE</p>
-							<p>Each symbol is a <a href="https://www.mathsisfun.com/hexadecimals.html"> hexadecmial number</a></p>
+							<p>Hex codes are a way of representing R(ed) G(reen) B(lue) colours.</p>
+							<p>Hex codes are split into 3 elements which are 2 symbols in length each.</p>
+							<p>First element represents RED, second element represents GREEN and the third element represents BLUE.</p>
+							<p>Each symbol is a <a href="https://www.mathsisfun.com/hexadecimals.html"> hexadecmial number.</a></p>
 							<p>For example #<span style={{textDecoration: "underline wavy red", color: "red"}}>aa</span><span style={{textDecoration: "underline wavy green", color: "green"}}>21</span><span style={{textDecoration: "underline wavy blue", colour: "blue"}}>df</span> mixes:</p>
-							<p>A red value of 170, a green value of 33 and a blue value of 223, which produces a <span style={{backgroundColor: "#aa21df", color: "#ffffff", textDecoration: "underline white"}}>blue-ish purple colour</span></p>
+							<p>A red value of 170, a green value of 33 and a blue value of 223, which produces a <span style={{backgroundColor: "#aa21df", color: "#ffffff", textDecoration: "underline white"}}>blue-ish purple colour.</span></p>
 						</div>
 					</Box>
 				</Modal>
