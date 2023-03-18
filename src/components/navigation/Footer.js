1425ad2310761751dc46495e325395486f642d83
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-03-18 15:04:51 +0000

    refactor help option from menu into another component

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { makeStyles  } from "@mui/styles"
-import { Grid, Menu, MenuItem, Button, Modal, Box, Checkbox, FormControlLabel } from "@mui/material"
+import HelpInfo from "../common/HelpInfo"
+import { Grid, Menu, MenuItem, Button, Modal, Checkbox, FormControlLabel } from "@mui/material"
 import { useHistory, useLocation } from "react-router-dom"
 import { useGlobalState } from "../../js/utils/globalState"
 
@@ -100,33 +101,6 @@ export default function Footer() {
 		},
 		spacing: {
 			marginTop: 50
-		},
-		helpModal: {
-			position: "absolute",
-			top: "50%",
-			left: "50%",
-			transform: "translate(-50%, -50%)",
-			width: 500,
-			maxWidth: "75%",
-			maxHeight: "60%",
-			overflow: "auto",
-			backgroundColor: "#ffffff",
-			boxShadow: 24,
-			padding: 25
-		},
-		helpModalTitle: {
-			fontFamily: "HelveticaBold",
-			fontSize: "3vh"
-		},
-		helpModalSubTitle: {
-			fontFamily: "HelveticaBold",
-			fontSize: "2.5vh",
-			paddingBottom: 5
-		},
-		helpModalText:{
-			fontFamily: "HelveticaLight",
-			fontSize: "2.2vh",
-			paddingBottom: 3
 		}
 	})
 
@@ -151,27 +125,7 @@ export default function Footer() {
 					aria-labelledby="modal-help-title"
 					aria-describedby="modal-help-description"
 				>
-					<Box className={classes.helpModal}>
-						<div id="modal-help-title" className={classes.helpModalTitle}>
-						Help
-						</div>
-						<br></br>
-						<div id="modal-help-description" className={classes.helpModalText} >
-							<div className={classes.helpModalSubTitle}> How to find Mixes </div>
-							<p>Each mix card clicks through to a page to read more information about a mix and listen to it.</p>
-							<p>You can see all mixes by navigating to the Menu and clicking <q>See All Mixes.</q></p>
-							<p>The colour grid and colour selector allow you to choose a colour that filters down to mixes of a similar hue.</p>
-							<p>You can either click a colour on the colour grid, or type in a hex code (6 characters) in the colour selector text field.</p>
-							<p>Colour differences are calculated using <a href="https://observablehq.com/@luciyer/euclidian-distance-in-rgb-color-space">Euclidean distance</a></p>
-							<div className={classes.helpModalSubTitle}> Hex Codes </div>
-							<p>Hex codes are a way of representing R(ed) G(reen) B(lue) colours.</p>
-							<p>Hex codes are split into 3 elements which are 2 symbols in length each.</p>
-							<p>First element represents RED, second element represents GREEN and the third element represents BLUE.</p>
-							<p>Each symbol is a <a href="https://www.mathsisfun.com/hexadecimals.html"> hexadecmial number.</a></p>
-							<p>For example #<span style={{textDecoration: "underline wavy red", color: "red"}}>aa</span><span style={{textDecoration: "underline wavy green", color: "green"}}>21</span><span style={{textDecoration: "underline wavy blue", colour: "blue"}}>df</span> mixes:</p>
-							<p>A red value of 170, a green value of 33 and a blue value of 223, which produces a <span style={{backgroundColor: "#aa21df", color: "#ffffff", textDecoration: "underline white"}}>blue-ish purple colour.</span></p>
-						</div>
-					</Box>
+					<HelpInfo/>
 				</Modal>
 				<div className={classes.spacing} />
 				<div className={classes.phantom} />
