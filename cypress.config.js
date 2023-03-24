babba755dd7c6fa4b12eb0ef5898e9f4c9f60de0
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-03-24 16:50:03 +0000

    Add a simple test for homepage with accessibility check
    Add config including baseUrl and setupNodeEvents function (which puts info of all events from checks in console log in a readable format)
    Add commands to do accessibility check
    Use accessibility check to fail e2e tests if any critical or serious violations are triggered

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl:  "http://localhost:3001",
+    video: false,
+    setupNodeEvents (on, _config) {
+      on("task", {
+        log (message) {
+          console.log(message)
+          return null
+        },
+        table (message) {
+          console.table(message)
+          return null
+        }
+      })
+    }
   },
-});
+})
