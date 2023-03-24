33271b4c4e6560793402e8d92c99912c6bc15122
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-03-24 16:50:03 +0000

    Add a simple test for homepage with accessibility check
    Add config including baseUrl and setupNodeEvents function (which puts info of all events from checks in console log in a readable format)
    Add commands to do accessibility check
    Use accessibility check to fail e2e tests if any critical or serious violations are triggered

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+import "cypress-axe"
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -10,16 +11,43 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: "element"}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: "optional"}, (subject, options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("a11yLog", (violations) => {
+  cy.task(
+    "log",
+    `${violations.length} accessibility violation${violations.length === 1 ? "" : "s"
+    } ${violations.length === 1 ? "was" : "were"} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task("table", violationData)
+})
+
+Cypress.Commands.add("customA11yCheck", (selector, logCallback) => {
+  const options = {
+    includedImpacts: ["critical", "serious"]
+  }
+  cy.injectAxe()
+  cy.checkA11y(selector, options, logCallback)
+})
