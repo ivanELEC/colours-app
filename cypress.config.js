da19416636ca784ec7444165af39da901707347e
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-04-16 14:18:15 +0100

    Add root url environment variable

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:  "http://localhost:3000",
+    baseUrl:  process.env.ROOT_URL,
     video: false,
     setupNodeEvents (on, _config) {
       on("task", {
