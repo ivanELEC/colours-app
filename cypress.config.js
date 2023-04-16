f6b01bf5b9b3335bfac147eb022bc3a50d768dc6
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2023-04-16 13:31:14 +0100

    Update test scripts

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:  "http://localhost:3002",
+    baseUrl:  "http://localhost:3000",
     video: false,
     setupNodeEvents (on, _config) {
       on("task", {
