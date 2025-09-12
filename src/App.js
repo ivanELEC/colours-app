2a06ebecc933d66e18c29bdf85d541b506cd3a58
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2025-09-12 02:00:16 +0100

    Add Firebase and Analytics - Add Custom Event for Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
 import ColourMatch from "./components/pages/ColourMatch"
 import "./App.css"
+import { logEvent } from "firebase/analytics"
+import { analytics } from "./index"
 
 
 function App() {
+	logEvent(analytics, "homepage_accessed")
 	return (
 		<div>
 			<ColourMatch/>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,38 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { GlobalStateProvider } from "./js/utils/globalState"
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
-import "./index.css"
+
 import Mix from "./components/pages/Mix"
 import App from "./App"
-//load fonts
+// Firebase/GA dependencies
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+//Styling
+import "./index.css"
+
+//Load fonts
 import "./fonts/HelveticaNeueCyr-Bold.ttf"
 import "./fonts/HelveticaNeueCyr-Light.ttf"
+
+
+
+//Firebase configuration
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 const routing = (
 	<div>
@@ -24,3 +50,5 @@ ReactDOM.render(
 	</GlobalStateProvider>,
 	document.getElementById("root")
 )
+
+export {analytics}
